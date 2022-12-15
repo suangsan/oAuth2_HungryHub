19b606954d31d7857ed1ebf8f13b7c75a62f20b3
Saiqul Haq <100819439+saiqulhaq-hh@users.noreply.github.com>
2022-12-15 17:28:38 +0700

    Update oauth2.js

--- oauth2.js
+++ oauth2.js
@@ -1,36 +1,32 @@
-const OAUTH2_PROVIDER = 'https://hhstaging.hungryhub.com';
+const OAUTH2_PROVIDER = 'hhstaging.hungryhub.com';
 const OAUTH2_CLIENT_ID = '0_fwWVrm8U_VtDPxPQ4oUZGChZfUrAZ_WagGXQeEhOA';
 const OAUTH2_CLIENT_SECRET = 'e7i_CWH8TQLev9cwAwODIw5sCCdI7Icp5DrdKqaP_1U';
 const OAUTH2_CALLBACK_URL = 'https://2b35-103-58-151-121.ap.ngrok.io/HungryHubCallBack';
+const code = null;
 
 let accessToken = null;
 let refreshToken = null;
 
-// Request an access token
-function requestAccessToken() {
+function requestCode() {
+  
   // Redirect the user to the OAuth2 provider's authorization endpoint
-  window.location = `https://${OAUTH2_PROVIDER}/oauth2/authorize?client_id=${OAUTH2_CLIENT_ID}&response_type=token&redirect_uri=${OAUTH2_CALLBACK_URL}`;
-}
+  let requestCodeUrl = `https://${OAUTH2_PROVIDER}/oauth/authorize?client_id=${OAUTH2_CLIENT_ID}&redirect_uri=${OAUTH2_CALLBACK_URL}&response_type=code&scope=dashboard_v2_owner-full+dashboard_v2_restaurant_group-full`
+  console.log(requestCodeUrl)
+  }
 
 // Handle the access token response from the OAuth2 provider
 function handleAccessTokenResponse() {
-  // Parse the access token and refresh token from the URL hash fragment
-//   const hash = window.location.hash.substr(1);
-//   const params = new URLSearchParams(hash);
-//   accessToken = params.get('access_token');
-//   refreshToken = params.get('refresh_token');
-
   // Exchange the access token for an access token and refresh token
   fetch(`https://${OAUTH2_PROVIDER}/partners/token.json`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: {
-      "code": "G72XQdb5eaVROL9g3fBBA_RXEJbowwLVHXhl3VCakPw",
+    body: JSON.stringify({
+      "code": "read from requestCode above",
       "grant_type": "authorization_code",
       "redirect_uri": OAUTH2_CALLBACK_URL
-    }
+    })
   }).then(response => {
     if (response.ok) {
       return response.json();
@@ -48,8 +44,7 @@ function handleAccessTokenResponse() {
 });
 }
 
-// Request an access token when the page loads
-requestAccessToken();
+requestCode();
 
 // Handle the access token response when the page is redirected
 handleAccessTokenResponse();
